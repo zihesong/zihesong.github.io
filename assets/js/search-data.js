58433c58e9c138fcee58388e418fd839b57d3e0c
zihesong <zoooesong@gmail.com>
2025-12-04 08:29:59 +0000

    Deploying to gh-pages from @ zihesong/zihesong.github.io@c70208ef5cb77defbe9daa3954ae0dc0583501bb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "Download my CV in English or Chinese below.",
+          description: "This is my Resume.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
