24346c5624ace2f5bca937129d5a046de7ef3361
zihesong <zoooesong@gmail.com>
2025-12-04 08:24:15 +0000

    Deploying to gh-pages from @ zihesong/zihesong.github.io@bf730fd618a8e5ecbee82bd4fa74d4290e754c60 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Teaching materials",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
