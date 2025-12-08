4d07d8652073fe1b7ca7beffb9a2587f22d54c44
zihesong <zoooesong@gmail.com>
2025-12-08 23:42:25 +0000

    Deploying to gh-pages from @ zihesong/zihesong.github.io@abbbd844e9cdfe12297508d8b4c40cb866ffab1f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,42 +459,21 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Resume_ZiheSong.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%7A%69%68%65.%73%6F%6E%67@%75%74%64%61%6C%6C%61%73.%65%64%75", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=FXfaN4EAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
